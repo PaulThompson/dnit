1c6becd06eab966a51b8ed3a39c1fa8f6fecc364
Paul Thompson <paul.thompson773@gmail.com>
2020-07-31 22:23:50 +1000

    Use std@0.62.0

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
-import * as flags from "https://deno.land/std@0.61.0/flags/mod.ts";
-import * as path from "https://deno.land/std@0.61.0/path/mod.ts";
-import * as log from "https://deno.land/std@0.61.0/log/mod.ts";
-import * as fs  from "https://deno.land/std@0.61.0/fs/mod.ts";
-import * as hash from "https://deno.land/std@0.61.0/hash/mod.ts";
+import * as flags from "https://deno.land/std@0.62.0/flags/mod.ts";
+import * as path from "https://deno.land/std@0.62.0/path/mod.ts";
+import * as log from "https://deno.land/std@0.62.0/log/mod.ts";
+import * as fs  from "https://deno.land/std@0.62.0/fs/mod.ts";
+import * as hash from "https://deno.land/std@0.62.0/hash/mod.ts";
 
 export {
   flags,
