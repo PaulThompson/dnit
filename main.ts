c577065bbf313cae0aaf449e67dd67f8235a33f0
Paul Thompson <paul.thompson773@gmail.com>
2020-10-19 14:38:58 +1100

    Add trivial (manual) version output util

--- main.ts
+++ main.ts
@@ -1,11 +1,16 @@
 import { setupLogging, log, flags } from "./mod.ts";
 import { launch } from "./launch.ts";
+import { version } from './version.ts';
 
 export async function main() {
+  const args = flags.parse(Deno.args);
+  if (args["version"] === true) {
+    console.log(`dnit ${version}`);
+    Deno.exit(0);
+  }
+
   await setupLogging();
   const intLogger = log.getLogger("dnit");
-
-  const args = flags.parse(Deno.args);
 
   if (args["verbose"] !== undefined) {
     intLogger.levelName = "INFO";
