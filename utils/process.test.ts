6c4166a711ef9bb1d8347d848baf188835793c07
Paul Thompson <paul.thompson773@gmail.com>
2021-06-25 15:09:46 +1000

    Dependency upgrades
    fix: semver@v1.4.0
    
    small update: std@0.92.0   (std@0.99.0 was too new)

--- utils/process.test.ts
+++ utils/process.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.91.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.92.0/testing/asserts.ts";
 
 import { processPipe, run } from "./process.ts";
 
