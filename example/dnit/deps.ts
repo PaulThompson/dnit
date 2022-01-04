0e459d3edb29d1ac83f5169affeb8659b3da1a5d
Paul Thompson <paul@helixta.com.au>
2022-01-04 16:07:25 +1100

    Commit release edits for dnit-v1.12.7

--- example/dnit/deps.ts
+++ example/dnit/deps.ts
@@ -2,7 +2,7 @@ import {
   exec,
   file,
   task,
-} from "https://deno.land/x/dnit@dnit-v1.12.6/dnit.ts";
+} from "https://deno.land/x/dnit@dnit-v1.12.7/dnit.ts";
 import * as flags from "https://deno.land/std@0.92.0/flags/mod.ts";
 import * as path from "https://deno.land/std@0.92.0/path/mod.ts";
 import * as log from "https://deno.land/std@0.92.0/log/mod.ts";
