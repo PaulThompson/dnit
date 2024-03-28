b697a1c2501d61a2800af04816198567a97e7044
Paul Thompson <paul.thompson773@gmail.com>
2024-03-28 20:08:07 +1100

    Update semver version

--- dnit/deps.ts
+++ dnit/deps.ts
@@ -6,6 +6,6 @@ import * as flags from "https://deno.land/std@0.221.0/flags/mod.ts";
 import * as path from "https://deno.land/std@0.221.0/path/mod.ts";
 import * as log from "https://deno.land/std@0.221.0/log/mod.ts";
 import * as fs from "https://deno.land/std@0.221.0/fs/mod.ts";
-import * as semver from "https://deno.land/x/semver@v1.4.0/mod.ts";
+import * as semver from "https://deno.land/x/semver@v1.4.1/mod.ts";
 
 export { file, flags, fs, log, main, path, semver, task, utils };
