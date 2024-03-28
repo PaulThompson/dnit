7423e330022f578e1b8f1fc817b5ee01d70dfb0e
Paul Thompson <paul.thompson773@gmail.com>
2024-03-28 20:08:51 +1100

    Commit release edits for dnit-v1.14.4

--- example/dnit/deps.ts
+++ example/dnit/deps.ts
@@ -2,7 +2,7 @@ import {
   file,
   main,
   task,
-} from "https://deno.land/x/dnit@dnit-v1.14.3/dnit.ts";
+} from "https://deno.land/x/dnit@dnit-v1.14.4/dnit.ts";
 import * as flags from "https://deno.land/std@0.221.0/flags/mod.ts";
 import * as path from "https://deno.land/std@0.221.0/path/mod.ts";
 import * as log from "https://deno.land/std@0.221.0/log/mod.ts";
