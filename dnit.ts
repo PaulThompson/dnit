bb8cd6025e5c3625ca62e2a723da451401213f3f
Paul Thompson <paul.thompson773@gmail.com>
2025-03-24 08:31:08 +1100

    fix: override keyword in dnit.ts on loggers

--- dnit.ts
+++ dnit.ts
@@ -615,14 +615,14 @@ class StdErrPlainHandler extends log.BaseHandler {
     });
   }
 
-  log(msg: string): void {
+  override log(msg: string): void {
     Deno.stderr.writeSync(new TextEncoder().encode(msg + "\n"));
   }
 }
 
 /// StdErr handler on top of ConsoleHandler (which uses colors)
 class StdErrHandler extends log.ConsoleHandler {
-  log(msg: string): void {
+  override log(msg: string): void {
     Deno.stderr.writeSync(new TextEncoder().encode(msg + "\n"));
   }
 }
