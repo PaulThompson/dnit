3fcae77c60a0759366525d506b26f317fb1fe567
Paul Thompson <paul.thompson773@gmail.com>
2025-03-24 08:30:22 +1100

    Use latest jsr std crypto

--- deps.ts
+++ deps.ts
@@ -2,7 +2,7 @@ import * as flags from "https://deno.land/std@0.221.0/flags/mod.ts";
 import * as path from "https://deno.land/std@0.221.0/path/mod.ts";
 import * as log from "https://deno.land/std@0.221.0/log/mod.ts";
 import * as fs from "https://deno.land/std@0.221.0/fs/mod.ts";
-import { crypto } from "https://deno.land/std@0.221.0/crypto/mod.ts";
+import { crypto } from "jsr:@std/crypto@1.0.4/crypto";
 import * as semver from "https://deno.land/x/semver@v1.4.1/mod.ts";
 
 export { crypto, flags, fs, log, path, semver };
