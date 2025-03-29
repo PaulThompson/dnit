24eea739d9b96ba325125a41bc60d083c34868af
Paul Thompson <paul.thompson773@gmail.com>
2025-03-29 15:40:05 +1100

    Add allow-import on the launch args

--- launch.ts
+++ launch.ts
@@ -149,6 +149,7 @@ export async function launch(logger: log.Logger): Promise<Deno.CommandStatus> {
       "--allow-run",
       "--allow-env",
       "--allow-net",
+      "--allow-import",
     ];
     const flags = [
       "--quiet",
