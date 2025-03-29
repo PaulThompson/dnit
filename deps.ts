2f0986c33780f7e3a5e86b9398aa87a205875550
Paul Thompson <paul.thompson773@gmail.com>
2025-03-29 15:09:01 +1100

    Update to jsr:@std/fs@1.0.15

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 import * as cli from "jsr:@std/cli@1.0.15/parse-args";
 import * as path from "jsr:@std/path@1.0.8";
 import * as log from "jsr:@std/log@0.224.14";
-import * as fs from "https://deno.land/std@0.221.0/fs/mod.ts";
+import * as fs from "jsr:@std/fs@1.0.15";
 import { crypto } from "jsr:@std/crypto@1.0.4/crypto";
 import * as semver from "https://deno.land/x/semver@v1.4.1/mod.ts";
 
