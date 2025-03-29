4c43dc9f5ac6005a75b105b25ef905a4a95b4f81
Paul Thompson <paul.thompson773@gmail.com>
2025-03-29 15:07:04 +1100

    Update to jsr:@std/log@0.224.14
    @std/log is likely to be removed in the future.
    https://github.com/denoland/std/issues/6124

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 import * as cli from "jsr:@std/cli@1.0.15/parse-args";
 import * as path from "jsr:@std/path@1.0.8";
-import * as log from "https://deno.land/std@0.221.0/log/mod.ts";
+import * as log from "jsr:@std/log@0.224.14";
 import * as fs from "https://deno.land/std@0.221.0/fs/mod.ts";
 import { crypto } from "jsr:@std/crypto@1.0.4/crypto";
 import * as semver from "https://deno.land/x/semver@v1.4.1/mod.ts";
