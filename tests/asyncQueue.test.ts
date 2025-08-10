d35087fdbcd71eb79ee2fe8953f3d2346d044ca4
Paul Thompson <paul.thompson773@gmail.com>
2025-08-11 08:48:00 +1000

    Rename TestHelper to TestConcurrency and simplify method names

--- tests/asyncQueue.test.ts
+++ tests/asyncQueue.test.ts
@@ -2,24 +2,24 @@ import { AsyncQueue } from "../utils/asyncQueue.ts";
 
 import { assert } from "@std/assert";
 
-class TestHelper {
+class TestConcurrency {
   numInProgress = 0;
   maxInProgress = 0;
 
-  incrementInProgress() {
+  start() {
     this.numInProgress += 1;
     this.maxInProgress = Math.max(this.maxInProgress, this.numInProgress);
   }
 
-  decrementInProgress() {
+  finish() {
     this.numInProgress -= 1;
   }
 
   action = () => {
-    this.incrementInProgress();
+    this.start();
     return new Promise<void>((resolve) => {
       setTimeout(() => {
-        this.decrementInProgress();
+        this.finish();
         resolve();
       }, 10);
     });
@@ -28,7 +28,7 @@ class TestHelper {
 
 Deno.test("async queue", async () => {
   for (let concurrency = 1; concurrency <= 32; concurrency *= 2) {
-    const ctx = new TestHelper();
+    const ctx = new TestConcurrency();
 
     const numTasks = concurrency * 10;
     const asyncQueue = new AsyncQueue(concurrency);
@@ -36,10 +36,8 @@ Deno.test("async queue", async () => {
     const promises: Promise<void>[] = [];
     for (let i = 0; i < numTasks; ++i) {
       promises.push(asyncQueue.schedule(ctx.action));
-      //promises.push(ctx.action()); // equivalent code but without the asyncQueue (runs them all in parallel)
     }
     await Promise.all(promises);
-    console.log(`maxInProgress: ${ctx.maxInProgress}`);
     assert(ctx.maxInProgress <= concurrency);
   }
 });
