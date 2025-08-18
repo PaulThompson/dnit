06b2d4da66effc4d563a3feb8d0a93ffa3d21df5
Paul Thompson <paul.thompson773@gmail.com>
2025-08-18 18:13:18 +1000

    Run CLI tests successfully with proper permissions

--- tests/cli.test.ts
+++ tests/cli.test.ts
@@ -1,5 +1,5 @@
 import { assertEquals, assertStringIncludes } from "@std/assert";
-import { execCli, task, runAlways } from "../mod.ts";
+import { execCli, runAlways, task } from "../mod.ts";
 
 Deno.test("CLI - execCli executes the requested task", async () => {
   let taskRun = false;
@@ -27,24 +27,28 @@ Deno.test("CLI - execCli defaults to list task when no args", async () => {
   });
 
   let output = "";
-  
+
   // run cli with no arg to test the 'list' feature on no args.
   await execCli([], [testTask], {
     stdout: (text: string) => {
       output += text;
-    }
+    },
   });
 
   assertStringIncludes(output, "myTask");
   assertStringIncludes(output, "My test task");
-  
+
   // check for all builtin tasks in the list
   assertStringIncludes(output, "clean");
   assertStringIncludes(output, "Clean tracked files");
-  
+
   assertStringIncludes(output, "list");
   assertStringIncludes(output, "List tasks");
-  
+
   assertStringIncludes(output, "tabcompletion");
   assertStringIncludes(output, "Generate shell completion script");
+});
+
+Deno.test("CLI - execCli handles non-existent task", async () => {
+  //
 });
