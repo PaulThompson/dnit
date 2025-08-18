d0cf2e46c34ba673a65f1869e7556adeeabda3ae
Paul Thompson <paul.thompson773@gmail.com>
2025-08-18 19:57:35 +1000

    drop junk tests

--- tests_junk/process.test.ts
+++
@@ -1,8 +0,0 @@
-import { assertEquals } from "@std/assert";
-
-import { run } from "../utils/process.ts";
-
-Deno.test("Process - run", async () => {
-  const str = await run(["echo", "hello world"]);
-  assertEquals(str.trim(), "hello world");
-});
